d9b303c3c21f5dee378f1aac8b68a80037820685
DesaiDixit123 <dixitdesai809@gmail.com>
2024-02-10 13:15:46 +0530

    main

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("hello").innerHTML = "hello javascript"
+document.querySelector(".mahi").innerHTML = "hello mahi"
+document.querySelector(".mahi").style.color = "red"
+document.querySelector(".div").innerHTML = "hello javascript"
+
+document.querySelector(".div").style.color = "blue"
+document.querySelector(".div").style.fontSize = "50px"
+document.querySelector(".div").style.backgroundColor = "black"
+document.querySelector(".div").style.textAlign = "center"
+alert("hello javascript")
+
+
+
+let a = "mahi"
+let b = "jatin"
+
+console.log(a + b)
+
+let c = 10
+let d = 20
+
+console.log(c + d)
+console.log(c - d)
+console.log(c * d)
+console.log(c / d)
+
+document.querySelector(".error").innerHTML = "error here"
